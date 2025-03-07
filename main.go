e46bbd21e6c959bdf4002226263de28e3677300f
joesepherus <joesepherus@gmail.com>
2025-03-07 18:56:25 +0800

    Add answersService to main.go.

--- main.go
+++ main.go
@@ -4,18 +4,12 @@ import (
 	"database/sql"
 	"qaa/controllers"
 	database "qaa/db"
+	"qaa/services/answersService"
 	"qaa/services/questionsService"
 	"qaa/templates"
 
 	_ "github.com/lib/pq"
 )
-
-type Answer struct {
-	ID         int    `json:"id"`
-	QuestionID int    `json:"question_id"`
-	UserAnswer string `json:"user_answer"`
-	Feedback   string `json:"feedback"` // New field for feedback
-}
 
 var db *sql.DB
 
@@ -27,31 +21,13 @@ func main() {
 	defer database.DB.Close()
 	// Pass the db connection to alertsService
 	questionsService.SetDB(db)
+	answersService.SetDB(db)
 
 	// start a new goroutine for the rest api endpoints
 	controllers.RestApi()
 
 }
 
-//func saveAnswer(w http.ResponseWriter, r *http.Request) {
-//	if r.Method != http.MethodPost {
-//		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-//		return
-//	}
-//
-//	var a Answer
-//	if err := json.NewDecoder(r.Body).Decode(&a); err != nil {
-//		http.Error(w, err.Error(), http.StatusBadRequest)
-//		return
-//	}
-//
-//	err := db.QueryRow(
-//		"INSERT INTO answers (question_id, user_answer) VALUES ($1, $2) RETURNING id",
-//		a.QuestionID, a.UserAnswer).Scan(&a.ID)
-//	if err != nil {
-//		http.Error(w, err.Error(), http.StatusInternalServerError)
-//		return
-//	}
 //
 //	w.Header().Set("Content-Type", "application/json")
 //	json.NewEncoder(w).Encode(a)
