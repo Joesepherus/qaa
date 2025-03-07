fd8896416e06871cb89fb94134d7b47cd401065b
joesepherus <joesepherus@gmail.com>
2025-03-07 19:47:47 +0800

    Make Answer field Feedback a pointer to string so it can hold null from db when there is nothing return just nil.

--- types/answersTypes/answersTypes.go
+++ types/answersTypes/answersTypes.go
@@ -6,6 +6,6 @@ type Answer struct {
 	ID         int    `json:"id"`
 	QuestionID int    `json:"question_id"`
 	UserAnswer string `json:"user_answer"`
-	Feedback   string `json:"feedback"`
+	Feedback   *string `json:"feedback"`
     CreatedAt  time.Time   `json:"created_at"`  
 }
