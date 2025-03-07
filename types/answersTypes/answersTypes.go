c153fc24fa5337981fe2cf2f5851e012b1d36731
joesepherus <joesepherus@gmail.com>
2025-03-07 18:55:36 +0800

    Add answersTypes and errorUtils.

---
+++ types/answersTypes/answersTypes.go
@@ -0,0 +1,11 @@
+package answersTypes
+
+import "time"
+
+type Answer struct {
+	ID         int    `json:"id"`
+	QuestionID int    `json:"question_id"`
+	UserAnswer string `json:"user_answer"`
+	Feedback   string `json:"feedback"`
+    CreatedAt  time.Time   `json:"created_at"`  
+}
