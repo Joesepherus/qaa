5b5d8d6b06d4fe69a68eaa2798799b2c2949d170
joesepherus <joesepherus@gmail.com>
2025-03-08 23:59:02 +0800

    Redirect to error page on all errors in trainingsController.

--- controllers/trainingsController/trainingsController.go
+++ controllers/trainingsController/trainingsController.go
@@ -1,7 +1,6 @@
 package trainingsController
 
 import (
-	"encoding/json"
 	"net/http"
 	"qaa/errorUtils"
 	"qaa/services/trainingsService"
@@ -10,14 +9,10 @@ import (
 func SaveTraining(w http.ResponseWriter, r *http.Request) {
 	errorUtils.MethodNotAllowed_error(w, r)
 
-	var response map[string]string
 	// Parse form values
 	err := r.ParseForm()
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		response = map[string]string{"error": "Failed to parse form data"}
-		json.NewEncoder(w).Encode(response)
+        http.Redirect(w, r, "/error?message=Failed+to+parse+form+data", http.StatusSeeOther)
 		return
 	}
 
@@ -27,28 +22,17 @@ func SaveTraining(w http.ResponseWriter, r *http.Request) {
 
 	// Example validation
 	if name == "" || description == "" {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		response = map[string]string{"error": "Invalid training data"}
-		json.NewEncoder(w).Encode(response)
+        http.Redirect(w, r, "/error?message=Failed+to+parse+form+data", http.StatusSeeOther)
 		return
 	}
 
 	// Add training to the database
 	_, err = trainingsService.SaveTraining(name, description)
 	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		response = map[string]string{"error": "Failed to store training"}
-		json.NewEncoder(w).Encode(response)
+		http.Redirect(w, r, "/error?message=Failed+to+save+training", http.StatusSeeOther)
 		return
 	}
 
 	// Success response
 	http.Redirect(w, r, "/training-saved", http.StatusSeeOther)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	response = map[string]string{"message": "Training added successfully"}
-	json.NewEncoder(w).Encode(response)
 }
