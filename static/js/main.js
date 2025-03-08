97fc0de9fda41ce0c31615c3a5fc09a347a086f5
joesepherus <joesepherus@gmail.com>
2025-03-08 22:34:23 +0800

    Add main.js.

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+function openCreateQuestionModal() {
+    const modal = document.getElementById("create-question-modal");
+    modal.classList.add("show");
+}
+
+function closeCreateQuestionModal() {
+    const modal = document.getElementById("create-question-modal");
+    modal.classList.remove("show");
+}
+
