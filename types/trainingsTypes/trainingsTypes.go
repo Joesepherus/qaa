6c886c22ee1b5eafa2cf6867f4a541276ff9ccc8
joesepherus <joesepherus@gmail.com>
2025-03-08 23:23:48 +0800

    Add trainingsTypes.

---
+++ types/trainingsTypes/trainingsTypes.go
@@ -0,0 +1,10 @@
+package trainingsTypes
+
+import "time"
+
+type Training struct {
+	ID         int       `json:"id"`
+	Name      string    `json:"name"`
+	Description string    `json:"description"`
+    CreatedAt  time.Time   `json:"created_at"`  
+}
